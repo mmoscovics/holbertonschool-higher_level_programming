dc4f154e8c2964a09b17ba935b1457fd0a6bb1da
Mitchell <mmoscovics@yahoo.com>
2019-12-10 17:26:55 +0000

    [Bugfix] abs for last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+ld = abs(number) % 10
 if number < 0:
-    ld = abs(number) % 10 * -1
-else:
-    ld = number % 10
+    ld = ld * -1
 if ld > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, ld))
 if ld is 0:
     print("Last digit of {} is {} and is 0".format(number, ld))
 if ld is not 0 and ld < 6:
     print("Last digit of {} is {} and is less than 6 \
-    and not 0".format(number, ld))
+and not 0".format(number, ld))
