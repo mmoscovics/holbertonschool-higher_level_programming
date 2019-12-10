227f8218002339f88e00679c70081f64dcfd2079
Mitchell <mmoscovics@yahoo.com>
2019-12-10 06:02:34 +0000

    [Create] task 3 print alphabet except q or e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Print ASCII alphabet in lowercase with out newline """
+
+
+for i in range (97, 122):
+    if i is 101 or i is 113:
+        continue
+    print("{:c}".format(i), end="")
