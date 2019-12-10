43a1e7b903c191e66699a5334e1bf888e16649f1
Mitchell <mmoscovics@yahoo.com>
2019-12-10 19:01:55 +0000

    [Bugfix] cap in FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,9 +7,7 @@ def fizzbuzz():
 
     for i in range(1, 101):
         if i % 15 is 0:
-            print("Fizzbuzz", end=" ")
-        elif i is 100:
-            print("Buzz", end="")
+            print("FizzBuzz", end=" ")
         elif i % 5 is 0:
             print("Buzz", end=" ")
         elif i % 3 is 0:
