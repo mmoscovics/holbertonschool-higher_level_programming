ab9c1a74aa94d6a62ea227c1bb702faaf1c2530c
Mitchell <mmoscovics@yahoo.com>
2019-12-10 16:58:00 +0000

    [Create] task 12 python fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" FIZZBUZZ numbers from 1 to 100 """
+
+
+def fizzbuzz():
+    """ Prints numbers from 1 to 100 with Fizzbuzz """
+
+    for i in range(1, 101):
+        if i % 15 is 0:
+            print("Fizzbuzz", end=" ")
+        elif i % 5 is 0:
+            print("Buzz", end=" ")
+        elif i % 3 is 0:
+            print("Fizz", end=" ")
+        else:
+            print(i, end=" ")
