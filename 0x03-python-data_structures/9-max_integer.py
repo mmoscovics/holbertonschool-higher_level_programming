2ee842a2accdb2e72fe05e2d18eef0443e2d1556
mmoscovics <mmoscovics@yahoo.com>
2019-12-17 18:03:38 +0000

    [Create] task 9 max int of list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Returns biggest int of a list """
+
+
+def max_integer(my_list=[]):
+    """ Returns biggest int """
+
+    max_int = my_list[0]
+
+    if list is '':
+        return None
+    for i in my_list:
+        if max_int < i:
+            max_int = i
+    return max_int
