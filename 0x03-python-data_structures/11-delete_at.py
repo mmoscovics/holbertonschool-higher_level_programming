bec11cd7bf9c97d154f7ede4d61af2e600c0bccf
mmoscovics <mmoscovics@yahoo.com>
2019-12-17 18:42:11 +0000

    [Bugfix] change return

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,6 +6,6 @@ def delete_at(my_list=[], idx=0):
     """ Deletes item of a list """
 
     if idx < 0 or idx >= len(my_list):
-        return None
+        return my_list
     del my_list[idx]
     return my_list
