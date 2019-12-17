99a1dce0508ebd3cad0ba4a44115ad3bfa25f852
mmoscovics <mmoscovics@yahoo.com>
2019-12-17 18:11:19 +0000

    [Update] change positioning

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,10 +5,9 @@
 def max_integer(my_list=[]):
     """ Returns biggest int """
 
-    max_int = my_list[0]
-
     if my_list is None or my_list is '':
         return None
+    max_int = my_list[0]
     for i in my_list:
         if max_int < i:
             max_int = i
