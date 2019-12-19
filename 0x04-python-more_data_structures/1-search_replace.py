d73b3021bb49af2f65a3722881d9ea8bdb892260
mmoscovics <mmoscovics@yahoo.com>
2019-12-19 16:07:19 +0000

    [Create] task 1 replace occurrences

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Replaces all occurrences of an element by another in a new list """
+
+
+def search_replace(my_list, search, replace):
+    """ Replaces element by another """
+
+    return [replace if i is search else i for i in my_list]
