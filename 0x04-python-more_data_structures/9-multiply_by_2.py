dda4ac084284a3005506c2acebace06be7c3bbb2
mmoscovics <mmoscovics@yahoo.com>
2019-12-19 20:30:13 +0000

    [Create] task 9 multiply values

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Returns new dictionary with all values multiplied by 2 """
+
+
+def multiply_by_2(a_dictionary):
+    """ Returns new dictionary with values multiplied by 2 """
+
+    new_dict = {}
+
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
