2c1dacacd9dc7e065110eec653817a7bd4118769
mmoscovics <mmoscovics@yahoo.com>
2019-12-19 17:38:19 +0000

    [Create] task 8 delete from dictionary

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Deletes a key in a dictionary """
+
+
+def simple_delete(a_dictionary, key=""):
+    """ Deletes key """
+
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
