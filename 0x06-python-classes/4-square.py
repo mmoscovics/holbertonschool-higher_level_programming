f7e8c0815395d9c7e3ac8fac83ef6baab9722de2
mmoscovics <mmoscovics@yahoo.com>
2020-01-09 18:10:25 +0000

    [Create] create getter and setter for size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Class Square that defines a square. """
+
+
+class Square:
+    """ Class Square. """
+
+    def __init__(self, size=0):
+        """ Initialize size of Class Square. """
+
+        self.size = size
+
+    def area(self):
+        """ Defines area of Class Square. """
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Gets and returns size after setter. """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets size to value after exception checks. """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
