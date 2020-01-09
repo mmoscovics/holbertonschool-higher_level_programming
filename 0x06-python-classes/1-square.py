f8c1c44a89268918cc8f2a2c45c4f9af47f78275
mmoscovics <mmoscovics@yahoo.com>
2020-01-09 18:08:58 +0000

    [Create] task 1 init size for class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class Square that defines a square. """
+
+
+class Square:
+    """ Class Square. """
+
+    def __init__(self, size):
+        """ Initialize size of square. """
+
+        self.__size = size
