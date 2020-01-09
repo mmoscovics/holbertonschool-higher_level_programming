a1247c398c0af8e4e2e01bd31cf17afcaaf21cf8
mmoscovics <mmoscovics@yahoo.com>
2020-01-09 18:09:33 +0000

    [Create] task 2 error handle size for class square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Class Square that defines a square. """
+
+
+class Square:
+    """ Class Square. """
+
+    def __init__(self, size=0):
+        """ Initialize size of square, checks size variable. """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
