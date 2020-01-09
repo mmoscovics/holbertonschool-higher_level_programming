48243b7a0834f13dbe2ddc694ddf7c5a66e7e82b
mmoscovics <mmoscovics@yahoo.com>
2020-01-09 18:11:43 +0000

    [Create] task 6 create getter and setter for position coordinates and update printing of class square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Class Square that defines a square. """
+
+
+class Square:
+    """ Class Square. """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initialize size of Class Square. """
+
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """ Defines area of Class Square. """
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Gets and returns size after setter. """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets size to value after exception checks. """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ Gets and returns position after setter. """
+
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ Sets position to value after exception checks. """
+
+        if type(value[0]) is not int and value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int and value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """ Prints the square with character #. """
+
+        if self.size is 0:
+            print()
+        else:
+            for j in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                print((" " * self.position[0]) + ("#" * self.size))
