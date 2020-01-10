13bf7eaf1b068fcc8b801550423f7f81923a8800
mmoscovics <mmoscovics@yahoo.com>
2020-01-10 20:48:36 +0000

    [Bugfix] pep8 complience

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,9 +41,9 @@ class Square:
     @position.setter
     def position(self, value):
         """ Sets position to value after exception checks. """
-        
-        if type(value) is not tuple or len(value) is not 2:
-            raise TypeError("position must be a tuple of 2 positive integers")     
+
+        if type(value) is not tuple and len(value) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int and value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[1]) is not int and value[1] < 0:
