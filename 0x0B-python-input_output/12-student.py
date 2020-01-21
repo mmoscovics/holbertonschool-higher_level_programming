65c45a9b25fb46f27a73a6c4943d7a4eeede8de7
mmoscovics <mmoscovics@yahoo.com>
2020-01-21 22:24:03 +0000

    [Update] use list comprehension for filter

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -19,6 +19,6 @@ class Student:
     def to_json(self, attrs=None):
         """ Retrieves a dictionary representation of a Student. """
 
-        if type(attrs) is list and type(attrs[0]) is str:
+        if type(attrs) is list and all([type(i) is str for i in attrs]):
             return {k: v for k, v in vars(self).items() if k in attrs}
         return vars(self)
