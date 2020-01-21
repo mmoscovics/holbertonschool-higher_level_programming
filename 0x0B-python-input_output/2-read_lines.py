f5da05a0dd5b10460857fcbe050c48b12aeb02cf
mmoscovics <mmoscovics@yahoo.com>
2020-01-21 19:07:13 +0000

    [Create] task 2 read n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Function that reads n lines of a text file and prints to stdout. """
+
+
+def read_lines(filename="", nb_lines=0):
+    """ Reads n lines of a text file and prints to stdout. """
+
+    if nb_lines <= 0:
+        with open(filename, 'r') as f:
+            print(f.read(), end='')
+    else:
+        with open(filename, 'r') as f:
+            for i in range(0, nb_lines):
+                print(f.readline(), end='')
