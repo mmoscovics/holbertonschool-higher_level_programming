c09754dcae48a481d0f32c6e125db335e60860bb
Mitchell <mmoscovics@yahoo.com>
2020-03-03 18:07:16 +0000

    [Bugfix] add quotes

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- list all privileges of the specified MYSQL users
 -- display user1
-SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- display user2
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
