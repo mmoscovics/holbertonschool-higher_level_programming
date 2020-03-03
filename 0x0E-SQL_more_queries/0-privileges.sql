07b3abc4385c35d7451092827e94260323ceda2e
Mitchell <mmoscovics@yahoo.com>
2020-03-03 18:08:53 +0000

    [Update] comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- list all privileges of the specified MYSQL users
--- display user1
+-- display users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- display user2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
