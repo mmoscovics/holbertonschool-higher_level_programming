869dc7195258895232aa81792991875d2e010cc1
Mitchell <mmoscovics@yahoo.com>
2020-03-03 19:34:11 +0000

    [Update] remove specifier

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- select database
 USE hbtn_0d_usa;
 -- creates table with variable restrictions
-CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 state_id INT NOT NULL, FOREIGN KEY(id) REFERENCES states(id),
 name VARCHAR(256) NOT NULL);
