ce500824883b26ef95159b86dba1ac0fb12996be
Mitchell <mmoscovics@yahoo.com>
2020-03-27 17:40:49 +0000

    [Create] task 2 specify filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
