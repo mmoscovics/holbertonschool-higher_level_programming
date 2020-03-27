764f0ce6486b11d90c5e0d15d4b2fde3c62b668a
Mitchell <mmoscovics@yahoo.com>
2020-03-27 21:23:05 +0000

    [Create] task 3 safe search

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists all states from a database with given search field"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE %s\
+                    ORDER BY id ASC", (argv[4],))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
