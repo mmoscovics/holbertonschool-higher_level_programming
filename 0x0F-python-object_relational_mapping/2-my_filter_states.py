4c37254e697367ff47e3aa99cb917a88eac0c71d
Mitchell <mmoscovics@yahoo.com>
2020-03-28 17:32:17 +0000

    [Bugfix] allow injection

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE states.name = '{}'\
+                ORDER BY id ASC".format(argv[4]))
     for row in cursor.fetchall():
         if row[1] == argv[4]:
             print(row)
