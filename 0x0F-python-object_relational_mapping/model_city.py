1ec08c7cb45a3c754053b1b5130fb450275a99bc
Mitchell <mmoscovics@yahoo.com>
2020-03-29 02:34:45 +0000

    [Create] task 14 print all city objects

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class definition for City and instance Base"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Class City that inherits Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
