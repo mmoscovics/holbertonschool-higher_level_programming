de3f55f5d90655d980b2469894ab0c50e73ad67f
Mitchell <mmoscovics@yahoo.com>
2020-04-13 18:00:19 +0000

    [Update] comments, tabs, spacing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Fetches url"""
+"""Fetches url with urllib"""
 from urllib import request
 
 
 if __name__ == "__main__":
-    """Fetches url 'https://intranet.hbtn.io/status'"""
     with request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- conent: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- conent: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
