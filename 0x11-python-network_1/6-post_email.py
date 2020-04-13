4cb9568ea352a7ed86e9943b0f6ad04357540381
Mitchell <mmoscovics@yahoo.com>
2020-04-13 21:46:42 +0000

    [Create] task 6 email with requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Takes URL and email, sends POST request to URL"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    email = {"email": argv[2]}
+    r = requets.post(argv[1], email)
+    print(r.text)
