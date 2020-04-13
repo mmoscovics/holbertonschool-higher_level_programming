6be4a542cfd74fba16b224e74b3367adb9c313dd
Mitchell <mmoscovics@yahoo.com>
2020-04-13 22:08:47 +0000

    [Create] task 7 error with requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Takes in URL, sends request to URL for error code"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    err = r.status_code
+    if code >= 400:
+        print("Error code: {}".format(err))
+    else:
+        print(r.text)
