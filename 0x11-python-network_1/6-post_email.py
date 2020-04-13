6f4826b5e2acce518295dd993c7326e612bcd393
Mitchell <mmoscovics@yahoo.com>
2020-04-13 21:57:52 +0000

    [Bugfix] typo

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ from sys import argv
 
 if __name__ == "__main__":
     email = {"email": argv[2]}
-    r = requets.post(argv[1], data=email)
+    r = requests.post(argv[1], data=email)
     print(r.text)
