1edf37c1b948df929211c08455b880d13f171a09
Mitchell <mmoscovics@yahoo.com>
2020-05-05 21:30:12 +0000

    [Update] typo

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const stringArray = ['C is fun', 'Python is coll', 'Javascript is amazing'];
+const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const i in stringArray) {
   console.log(stringArray[i]);
 }
