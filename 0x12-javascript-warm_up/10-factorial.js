8df102410213f65977667cd826f0aa86ff344e95
Mitchell <mmoscovics@yahoo.com>
2020-05-05 23:51:46 +0000

    [Update] semistandard compliance

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 function factorial (number) {
   if (isNaN(number)) {
     return 1;
-  } else if (number <=1) {
+  } else if (number <= 1) {
     return 1;
   } else {
     return (number * factorial(number - 1));
