d8015e35ca6b45aaf1d41fb0b6031c8a124dce20
Mitchell <mmoscovics@yahoo.com>
2020-05-06 00:37:39 +0000

    [Update] semistandard compliance

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ const args = process.argv.slice(2);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  args.sort(function (a, b) { return a - b }).reverse();
+  args.sort(function (a, b) { return a - b; }).reverse();
   console.log(args[1]);
 }
