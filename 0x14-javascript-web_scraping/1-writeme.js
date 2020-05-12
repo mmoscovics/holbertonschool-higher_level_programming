53d7ba49a557ee69103498e7bc2598ede576df1c
Mitchell <mmoscovics@yahoo.com>
2020-05-12 20:47:26 +0000

    [Create] task 1 write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.slice(2);
+fs.writeFile(args[0], args[1], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+});
