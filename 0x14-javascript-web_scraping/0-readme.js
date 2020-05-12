ac101feafa282061d438455e76859ba814bc6365
Mitchell <mmoscovics@yahoo.com>
2020-05-12 20:22:02 +0000

    [Create] task 0 reads and prints file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv.slice(2);
+fs.readFile(args[0], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
